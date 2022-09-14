155a735fa70872178c56a03a96a932247d32ae43
a-mikhaylov <lexarobotr2d2@gmail.com>
2022-09-14 19:09:03 +0300

    use macros

--- header.hpp
+++ header.hpp
@@ -2,38 +2,94 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <chrono>
 #include <nlohmann/json.hpp>
 
-#define OFILE_NAME "my_son.json"
-#define IFILE_NAME "test.json"
+#define DIF_NAME    "my_dif.json"
+#define MEAN_NAME   "my_mean.json"
 
 using json = nlohmann::json;
+using namespace std;
+using namespace std::chrono;
 
-class SlideMeanFilter {
-public:
-	std::vector<int32_t> buf;
-	int32_t k;
-	bool need_restart;
-	int window;
-	int mean_SUM;
+namespace mean {
+    class SlideMeanFilter {
+    public:
+    	vector<int32_t>     buf;
+    	int32_t             k;
+    	bool                need_restart;
+    	int                 window;
+    	int                 mean_SUM;
 
-	SlideMeanFilter(int window) {
-		this->window        = window;
-		this->buf.resize(window, 0);
-		this->k             = 123;
-		this->need_restart  = true;
-        this->mean_SUM      = 456;
-	}
-    SlideMeanFilter() {
-		this->window        = 0;
-		this->buf           = {};
-		this->k             = 987;
-		this->need_restart  = false;
-        this->mean_SUM      = 654;
-	}
-};
+    	SlideMeanFilter(int window) {
+    		this->window        = window;
+    		this->buf.resize(window, 0);
+    		this->k             = 123;
+    		this->need_restart  = true;
+            this->mean_SUM      = 456;
+    	}
 
-json mean_to_json_convert(SlideMeanFilter smf) {
+        SlideMeanFilter() {
+    		this->window        = 0;
+    		this->buf           = {};
+    		this->k             = 987;
+    		this->need_restart  = false;
+            this->mean_SUM      = 654;
+    	}
+
+        void printSMF() {
+            std::cerr << endl;
+            std::cerr << "k:\t\t" <<this->k << std::endl;
+            std::cerr << "window:\t\t" <<this->window << std::endl;
+            std::cerr << "mean_sum:\t" <<this->mean_SUM << std::endl;
+            std::cerr << "need_rest:\t" <<this->need_restart << std::endl;
+            std::cerr << "buf:\t\t";
+            for (int i = 0; i < this->buf.size(); i++) {
+                std::cerr << this->buf[i] << " ";
+            }
+            std::cerr << "\n\n";
+        }
+    };
+    
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(SlideMeanFilter, buf, k, need_restart,
+                                        window, mean_SUM);
+}
+
+namespace dif {
+    class DifFilter {
+    public:
+        int         mama;
+        int         papa;
+        vector<int> iog_satot;
+        string      dworf;
+
+        DifFilter() {
+            this->mama = 0;
+            this->papa = 1;
+            this->iog_satot = {123, 456, 888, 5560046};
+            this->dworf = "Gimly";
+        }
+
+        void printDIF() {
+            cerr << "mama:\t" << mama << endl;
+            cerr << "papa:\t" << papa << endl;
+            cerr << "dworf:\t" << dworf << endl;
+            cerr << "iog_satot:\t";
+            for (int i = 0; i < iog_satot.size(); i++) {
+                cerr << iog_satot[i] << " ";
+            }
+            cerr << "\n\n";
+        }
+    };
+
+    NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(DifFilter, mama, papa,
+                                        iog_satot, dworf);
+}
+
+using namespace mean;
+using namespace dif;
+
+json                    mean_to_json_convert(mean::SlideMeanFilter smf) {
     json flt;
 
     flt["buf"]          = smf.buf;
@@ -45,8 +101,8 @@ json mean_to_json_convert(SlideMeanFilter smf) {
     return flt;
 }
 
-SlideMeanFilter json_to_mean_convert(json j) {
-    SlideMeanFilter flt;
+mean::SlideMeanFilter   json_to_mean_convert(json j) {
+    mean::SlideMeanFilter flt;
     
     flt.k               = j["k"];
     flt.mean_SUM        = j["mean_SUM"];
@@ -61,13 +117,13 @@ SlideMeanFilter json_to_mean_convert(json j) {
     return flt;
 }
 
-void serealize_json(json j, std::string fname) {
+void                    serealize_json(json j, std::string fname) {
     std::ofstream out(fname);
 
     out << j.dump(4) << std::endl;
 }
 
-json deserealize_json(std::string fname) {
+json                    deserealize_json(std::string fname) {
     std::ifstream inp(fname);
     json ji;
     
@@ -75,11 +131,11 @@ json deserealize_json(std::string fname) {
     return ji;
 }
 
-std::string json_data_str(std::string before, std::vector<int> to_add) {
-    for (int i = 0; i < to_add.size(); i++) {
-        before += " ";
-        before += std::to_string(to_add[i]);
-    }
+template<typename T>
+void serialize(T state, string fname) {
+    json j;
+    to_json(j, state);
+    std::ofstream out(fname);
 
-    return before;
+    out << j.dump(4) << std::endl;
 }
